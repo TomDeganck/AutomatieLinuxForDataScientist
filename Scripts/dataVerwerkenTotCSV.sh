01986e702f47cca88f9dff6c61fafe8d4ff73c60
TomDeganck <tom.deganck@student.hogent.be>
2023-11-27 11:39:38 +0100

    codeveranderd

--- Scripts/dataVerwerkenTotCSV.sh
+++ Scripts/dataVerwerkenTotCSV.sh
@@ -11,11 +11,11 @@ output_csv="output.csv"
 echo "Tijdstip,Locatie,Temperatuur,Vochtigheid,Dag,Windsnelheid,Windrichting,Gevoelstemperatuur,UV index" > "$output_csv"
 
 # Process JSON files
-for json_file in data_storage/*.json; do
+for json_file in ../data_storage/*.json; do
     timestamp=$(jq -r '.localtime' "$json_file")
-    Locatie=$(jq -r '.name' "$json_file")
+    locatie=$(jq -r '.name' "$json_file")
     temp=$(jq -r '.temp_c' "$json_file")
     humidity=$(jq -r '.humidity' "$json_file")
 
-    echo "$timestamp,$co2,$temp,$humidity" >> "$output_csv"
+    echo "$timestamp,$locatie,$temp,$humidity" >> "$output_csv"
 done
