71b0a08a18cf2b7d997027bcf80055334ea652db
Tom Deganck <tom.deganck@student.hogent.be>
2023-11-27 12:10:24 +0100

    proberen debuggen warrom bepaalde bestanden niet inladen

--- Scripts/dataVerwerkenTotCSV.sh
+++ Scripts/dataVerwerkenTotCSV.sh
@@ -8,22 +8,27 @@ set -o pipefail
 output_csv="output.csv"
 
 # Write CSV header
-echo "Tijdstip,Locatie,Temperatuur_C,Temperatuur_F,Vochtigheid,Dag,Windsnelheid_kph,Windsnelheid_mph,Windrichting,Gevoelstemperatuur_C,Gevoelstemperatuur_F,UV index" > "$output_csv"
+echo "tijdstip,localtime,Locatie,Temperatuur_C,Temperatuur_F,Vochtigheid,Dag,Windsnelheid_kph,Windsnelheid_mph,Windrichting,Gevoelstemperatuur_C,Gevoelstemperatuur_F,UV index" > "$output_csv"
 
 # Process JSON files
 for json_file in ../data_storage/*.json; do
-    timestamp=$(jq '.location.localtime' "$json_file")
-    locatie=$(jq '.location.name' "$json_file")
-    temp_c=$(jq '.current.temp_c' "$json_file")
-    temp_f=$(jq '.current.temp_f' "$json_file")
-    humidity=$(jq '.current.humidity' "$json_file")
-    isDay=$(jq '.current.is_day' "$json_file")
-    windSpeed_kph=$(jq '.current.wind_kph' "$json_file")
-    windSpeed_mph=$(jq '.current.wind_mph' "$json_file")
-    windDirection=$(jq '.current.wind_dir' "$json_file")
-    feelTemperatuur_c=$(jq '.current.feelslike_c' "$json_file")
-    feelTemperatuur_f=$(jq '.current.feelslike_f' "$json_file")
-    UVindx=$(jq '.current.uv' "$json_file")
+    if [ -f "$json_file" ]; then  # Check if it's a regular file
 
-    echo "$timestamp,$locatie,$temp_c,$temp_f,$humidity,$isDay,$windSpeed_kph,$windSpeed_mph,$windDirection,$feelTemperatuur_c,$feelTemperatuur_f,$UVindx" >> "$output_csv"
+        timestamp=$(jq '.location.localtime' "$json_file")
+        locatie=$(jq '.location.name' "$json_file")
+        temp_c=$(jq '.current.temp_c' "$json_file")
+        temp_f=$(jq '.current.temp_f' "$json_file")
+        humidity=$(jq '.current.humidity' "$json_file")
+        isDay=$(jq '.current.is_day' "$json_file")
+        windSpeed_kph=$(jq '.current.wind_kph' "$json_file")
+        windSpeed_mph=$(jq '.current.wind_mph' "$json_file")
+        windDirection=$(jq '.current.wind_dir' "$json_file")
+        feelTemperatuur_c=$(jq '.current.feelslike_c' "$json_file")
+        feelTemperatuur_f=$(jq '.current.feelslike_f' "$json_file")
+        UVindx=$(jq '.current.uv' "$json_file")
+
+        echo "$timestamp,$locatie,$temp_c,$temp_f,$humidity,$isDay,$windSpeed_kph,$windSpeed_mph,$windDirection,$feelTemperatuur_c,$feelTemperatuur_f,$UVindx" >> "$output_csv"
+    else
+        echo "Skipping non-regular file: $json_file"
+    fi
 done
