c7d3a55456dec774580600933267605007671862
Tom Deganck <tom.deganck@student.hogent.be>
2023-11-27 11:48:30 +0100

    CSV werkt

--- Scripts/dataVerwerkenTotCSV.sh
+++ Scripts/dataVerwerkenTotCSV.sh
@@ -13,11 +13,9 @@ echo "Tijdstip,Locatie,Temperatuur,Vochtigheid,Dag,Windsnelheid,Windrichting,Gev
 # Process JSON files
 for json_file in ../data_storage/*.json; do
     timestamp=$(jq '.location.localtime' "$json_file")
-    locatie=$(jq '.name' "$json_file")
-    temp=$(jq  '.temp_c' "$json_file")
-    humidity=$(jq '.humidity' "$json_file")
+    locatie=$(jq '.location.name' "$json_file")
+    temp=$(jq  '.current.temp_c' "$json_file")
+    humidity=$(jq '.current.humidity' "$json_file")
     
-    echo "Timestamp: $timestamp, Location: $locatie, Temperature: $temp, Humidity: $humidity"
-
     echo "$timestamp,$locatie,$temp,$humidity" >> "$output_csv"
 done
