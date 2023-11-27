577e94a64511cf0db324d4771c4e9cdfc368935f
Tom Deganck <tom.deganck@student.hogent.be>
2023-11-27 11:33:22 +0100

    cvs bijgewerkt

--- Scripts/dataVerwerkenTotCSV.sh
+++ Scripts/dataVerwerkenTotCSV.sh
@@ -8,13 +8,13 @@ set -o pipefail
 output_csv="output.csv"
 
 # Write CSV header
-echo "tijdStip,Locatie,Temperatuur,Vochtigheid,Dag,Windsnelheid,Windrichting,Gevoelstemperatuur,UV index" > "$output_csv"
+echo "Tijdstip,Locatie,Temperatuur,Vochtigheid,Dag,Windsnelheid,Windrichting,Gevoelstemperatuur,UV index" > "$output_csv"
 
 # Process JSON files
 for json_file in data_storage/*.json; do
-    timestamp=$(jq -r '.timestamp' "$json_file")
-    co2=$(jq -r '.co2' "$json_file")
-    temp=$(jq -r '.temp' "$json_file")
+    timestamp=$(jq -r '.localtime' "$json_file")
+    Locatie=$(jq -r '.name' "$json_file")
+    temp=$(jq -r '.temp_c' "$json_file")
     humidity=$(jq -r '.humidity' "$json_file")
 
     echo "$timestamp,$co2,$temp,$humidity" >> "$output_csv"
