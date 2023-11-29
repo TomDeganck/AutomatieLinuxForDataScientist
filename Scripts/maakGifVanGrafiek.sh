a35cfac7ece4f4809b64983d102ab680b6d3182c
TomDeganck <114476801+TomDeganck@users.noreply.github.com>
2023-11-29 20:12:11 +0100

    Update maakGifVanGrafiek.sh

--- Scripts/maakGifVanGrafiek.sh
+++ Scripts/maakGifVanGrafiek.sh
@@ -8,6 +8,6 @@ input_folder="./images"
 output_gif="grafiek.gif"
 
 
-convert -delay 100 -loop 0 "$input_folder/"*.jpg "$output_gif"
+convert -delay 100 -loop 0 "$input_folder/"*.jpg "$input_folder/$output_gif"
 
 echo "GIF created: $output_gif"
